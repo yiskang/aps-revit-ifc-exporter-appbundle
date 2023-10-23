c2f769ea72e6c727e0110d0e85ff6dc25a9d163a
Eason Kang <eason.kang@autodesk.com>
2023-10-23 11:20:34 +0800

    Add utility function of converting IFC export configurations between the legacy format and JSON-based formt (introduced since v22.2.0.0).

--- RevitIfcExporter/IFC/IFCExportConfigurationsMap.partial.cs
+++ RevitIfcExporter/IFC/IFCExportConfigurationsMap.partial.cs
@@ -19,10 +19,19 @@
 // ref: https://github.com/Autodesk/revit-ifc/blob/9a6fc912fb8510647f9d2bcc0d57b0451a2194b6/Source/IFCExporterUIOverride/IFCExportConfigurationsMap.cs
 //
 
-using Autodesk.Revit.DB;
-using Newtonsoft.Json;
-using RevitIfcExporter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.IO;
+
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.IFC;
+using Autodesk.Revit.DB.ExtensibleStorage;
+using Revit.IFC.Common.Enums;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
 
 namespace BIM.IFC.Export
 {
@@ -78,6 +87,330 @@ namespace BIM.IFC.Export
             this.AddOrReplace(configuration);
 
             return configuration.Name;
+        }
+
+        public bool ConvertSavedConfigurationsFromJsonToLegacy(Document document)
+        {
+            if (m_jsonSchema == null)
+            {
+                m_jsonSchema = Schema.Lookup(s_jsonSchemaId);
+            }
+            if (m_jsonSchema != null)
+            {
+                IList<DataStorage> oldSavedConfigurations = GetSavedConfigurations(m_jsonSchema, document);
+                if (oldSavedConfigurations.Count > 0)
+                {
+                    Transaction deleteTransaction = new Transaction(document, "Delete JSON-based IFC export setups");
+                    try
+                    {
+                        deleteTransaction.Start("Delete JSON-based configuration");
+                        List<ElementId> dataStorageToDelete = new List<ElementId>();
+                        foreach (DataStorage dataStorage in oldSavedConfigurations)
+                        {
+                            dataStorageToDelete.Add(dataStorage.Id);
+                        }
+                        document.Delete(dataStorageToDelete);
+                        deleteTransaction.Commit();
+                    }
+                    catch (System.Exception)
+                    {
+                        if (deleteTransaction.HasStarted())
+                            deleteTransaction.RollBack();
+
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+
+                // update the configurations to new map schema.
+                if (m_mapSchema == null)
+                {
+                    m_mapSchema = Schema.Lookup(s_mapSchemaId);
+                }
+
+                // Are there any setups to save or resave?
+                List<IFCExportConfiguration> setupsToSave = new List<IFCExportConfiguration>();
+                foreach (IFCExportConfiguration configuration in m_configurations.Values)
+                {
+                    if (configuration.IsBuiltIn)
+                        continue;
+
+                    // Store in-session settings in the cached in-session configuration
+                    if (configuration.IsInSession)
+                    {
+                        IFCExportConfiguration.SetInSession(configuration);
+                        continue;
+                    }
+
+                    setupsToSave.Add(configuration);
+                }
+
+                // If there are no setups to save, and if the schema is not present (which means there are no
+                // previously existing setups which might have been deleted) we can skip the rest of this method.
+                if (setupsToSave.Count <= 0 && m_mapSchema == null)
+                    return false;
+
+                if (m_mapSchema == null)
+                {
+                    SchemaBuilder builder = new SchemaBuilder(s_mapSchemaId);
+                    builder.SetSchemaName("IFCExportConfigurationMap");
+                    builder.AddMapField(s_configMapField, typeof(String), typeof(String));
+                    m_mapSchema = builder.Finish();
+                }
+
+                // Overwrite all saved configs with the new list
+                Transaction transaction = new Transaction(document, "Update IFC export setups");
+                try
+                {
+                    transaction.Start();
+                    IList<DataStorage> savedConfigurations = GetSavedConfigurations(m_mapSchema, document);
+                    int savedConfigurationCount = savedConfigurations.Count<DataStorage>();
+                    int savedConfigurationIndex = 0;
+                    foreach (IFCExportConfiguration configuration in setupsToSave)
+                    {
+                        DataStorage configStorage;
+                        if (savedConfigurationIndex >= savedConfigurationCount)
+                        {
+                            configStorage = DataStorage.Create(document);
+                        }
+                        else
+                        {
+                            configStorage = savedConfigurations[savedConfigurationIndex];
+                            savedConfigurationIndex++;
+                        }
+
+                        Entity mapEntity = new Entity(m_mapSchema);
+                        IDictionary<string, string> mapData = new Dictionary<string, string>();
+                        mapData.Add(s_setupName, configuration.Name);
+                        mapData.Add(s_setupVersion, configuration.IFCVersion.ToString());
+                        mapData.Add(s_exchangeRequirement, configuration.ExchangeRequirement.ToString());
+                        mapData.Add(s_setupFileFormat, configuration.IFCFileType.ToString());
+                        mapData.Add(s_setupSpaceBoundaries, configuration.SpaceBoundaries.ToString());
+                        mapData.Add(s_setupQTO, configuration.ExportBaseQuantities.ToString());
+                        mapData.Add(s_setupCurrentView, configuration.VisibleElementsOfCurrentView.ToString());
+                        mapData.Add(s_splitWallsAndColumns, configuration.SplitWallsAndColumns.ToString());
+                        mapData.Add(s_setupExport2D, configuration.Export2DElements.ToString());
+                        mapData.Add(s_setupExportRevitProps, configuration.ExportInternalRevitPropertySets.ToString());
+                        mapData.Add(s_setupExportIFCCommonProperty, configuration.ExportIFCCommonPropertySets.ToString());
+                        mapData.Add(s_setupUse2DForRoomVolume, configuration.Use2DRoomBoundaryForVolume.ToString());
+                        mapData.Add(s_setupUseFamilyAndTypeName, configuration.UseFamilyAndTypeNameForReference.ToString());
+                        mapData.Add(s_setupExportPartsAsBuildingElements, configuration.ExportPartsAsBuildingElements.ToString());
+                        mapData.Add(s_useActiveViewGeometry, configuration.UseActiveViewGeometry.ToString());
+                        mapData.Add(s_setupExportSpecificSchedules, configuration.ExportSpecificSchedules.ToString());
+                        mapData.Add(s_setupExportBoundingBox, configuration.ExportBoundingBox.ToString());
+                        mapData.Add(s_setupExportSolidModelRep, configuration.ExportSolidModelRep.ToString());
+                        mapData.Add(s_setupExportSchedulesAsPsets, configuration.ExportSchedulesAsPsets.ToString());
+                        mapData.Add(s_setupExportUserDefinedPsets, configuration.ExportUserDefinedPsets.ToString());
+                        mapData.Add(s_setupExportUserDefinedPsetsFileName, configuration.ExportUserDefinedPsetsFileName);
+                        mapData.Add(s_setupExportUserDefinedParameterMapping, configuration.ExportUserDefinedParameterMapping.ToString());
+                        mapData.Add(s_setupExportUserDefinedParameterMappingFileName, configuration.ExportUserDefinedParameterMappingFileName);
+                        mapData.Add(s_setupExportLinkedFiles, configuration.ExportLinkedFiles.ToString());
+                        mapData.Add(s_setupIncludeSiteElevation, configuration.IncludeSiteElevation.ToString());
+                        mapData.Add(s_setupStoreIFCGUID, configuration.StoreIFCGUID.ToString());
+                        mapData.Add(s_setupActivePhase, configuration.ActivePhaseId.ToString());
+                        mapData.Add(s_setupExportRoomsInView, configuration.ExportRoomsInView.ToString());
+                        mapData.Add(s_useOnlyTriangulation, configuration.UseOnlyTriangulation.ToString());
+                        mapData.Add(s_excludeFilter, configuration.ExcludeFilter.ToString());
+                        mapData.Add(s_setupSitePlacement, configuration.SitePlacement.ToString());
+                        mapData.Add(s_useTypeNameOnlyForIfcType, configuration.UseTypeNameOnlyForIfcType.ToString());
+                        mapData.Add(s_useVisibleRevitNameAsEntityName, configuration.UseVisibleRevitNameAsEntityName.ToString());
+                        mapData.Add(s_setupTessellationLevelOfDetail, configuration.TessellationLevelOfDetail.ToString());
+                        // For COBie v2.4
+                        mapData.Add(s_cobieCompanyInfo, configuration.COBieCompanyInfo);
+                        mapData.Add(s_cobieProjectInfo, configuration.COBieProjectInfo);
+                        mapData.Add(s_includeSteelElements, configuration.IncludeSteelElements.ToString());
+                        // Geo Reference info
+                        mapData.Add(s_geoRefCRSName, string.IsNullOrEmpty(configuration.GeoRefCRSName) ? string.Empty : configuration.GeoRefCRSName);
+                        mapData.Add(s_geoRefCRSDesc, string.IsNullOrEmpty(configuration.GeoRefCRSDesc) ? string.Empty : configuration.GeoRefCRSName);
+                        mapData.Add(s_geoRefEPSGCode, string.IsNullOrEmpty(configuration.GeoRefEPSGCode) ? string.Empty : configuration.GeoRefCRSName);
+                        mapData.Add(s_geoRefGeodeticDatum, string.IsNullOrEmpty(configuration.GeoRefGeodeticDatum) ? string.Empty : configuration.GeoRefCRSName);
+                        mapData.Add(s_geoRefMapUnit, string.IsNullOrEmpty(configuration.GeoRefMapUnit) ? string.Empty : configuration.GeoRefCRSName);
+
+                        mapEntity.Set<IDictionary<string, String>>(s_configMapField, mapData);
+                        configStorage.SetEntity(mapEntity);
+                    }
+
+                    List<ElementId> elementsToDelete = new List<ElementId>();
+                    for (; savedConfigurationIndex < savedConfigurationCount; savedConfigurationIndex++)
+                    {
+                        DataStorage configStorage = savedConfigurations[savedConfigurationIndex];
+                        elementsToDelete.Add(configStorage.Id);
+                    }
+                    if (elementsToDelete.Count > 0)
+                        document.Delete(elementsToDelete);
+
+                    transaction.Commit();
+                }
+                catch (System.Exception)
+                {
+                    if (transaction.HasStarted())
+                        transaction.RollBack();
+
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool ConvertSavedConfigurationsFromLegacyToJson(Document document)
+        {
+            // delete the old schema and the DataStorage.
+            if (m_OldSchema == null)
+            {
+                m_OldSchema = Schema.Lookup(s_OldSchemaId);
+            }
+            if (m_OldSchema != null)
+            {
+                IList<DataStorage> oldSavedConfigurations = GetSavedConfigurations(m_OldSchema, document);
+                if (oldSavedConfigurations.Count > 0)
+                {
+                    Transaction deleteTransaction = new Transaction(document, "Delete old IFC export setups");
+                    try
+                    {
+                        deleteTransaction.Start("Delete old configuration");
+                        List<ElementId> dataStorageToDelete = new List<ElementId>();
+                        foreach (DataStorage dataStorage in oldSavedConfigurations)
+                        {
+                            dataStorageToDelete.Add(dataStorage.Id);
+                        }
+                        document.Delete(dataStorageToDelete);
+                        deleteTransaction.Commit();
+                    }
+                    catch (System.Exception)
+                    {
+                        if (deleteTransaction.HasStarted())
+                            deleteTransaction.RollBack();
+
+                        return false;
+                    }
+                }
+            }
+
+            // delete the old schema and the DataStorage.
+            if (m_mapSchema == null)
+            {
+                m_mapSchema = Schema.Lookup(s_mapSchemaId);
+            }
+            if (m_mapSchema != null)
+            {
+                IList<DataStorage> oldSavedConfigurations = GetSavedConfigurations(m_mapSchema, document);
+                if (oldSavedConfigurations.Count > 0)
+                {
+                    Transaction deleteTransaction = new Transaction(document, "Delete old configuration");
+                    try
+                    {
+                        deleteTransaction.Start("Delete old configuration");
+                        List<ElementId> dataStorageToDelete = new List<ElementId>();
+                        foreach (DataStorage dataStorage in oldSavedConfigurations)
+                        {
+                            dataStorageToDelete.Add(dataStorage.Id);
+                        }
+                        document.Delete(dataStorageToDelete);
+                        deleteTransaction.Commit();
+                    }
+                    catch (System.Exception)
+                    {
+                        if (deleteTransaction.HasStarted())
+                            deleteTransaction.RollBack();
+
+                        return false;
+                    }
+                }
+            }
+
+            // update the configurations to new map schema.
+            if (m_jsonSchema == null)
+            {
+                m_jsonSchema = Schema.Lookup(s_jsonSchemaId);
+            }
+
+            // Are there any setups to save or resave?
+            List<IFCExportConfiguration> setupsToSave = new List<IFCExportConfiguration>();
+            foreach (IFCExportConfiguration configuration in m_configurations.Values)
+            {
+                if (configuration.IsBuiltIn)
+                    continue;
+
+                // Store in-session settings in the cached in-session configuration
+                if (configuration.IsInSession)
+                {
+                    IFCExportConfiguration.SetInSession(configuration);
+                    continue;
+                }
+
+                setupsToSave.Add(configuration);
+            }
+
+            // If there are no setups to save, and if the schema is not present (which means there are no
+            // previously existing setups which might have been deleted) we can skip the rest of this method.
+            if (setupsToSave.Count <= 0 && m_jsonSchema == null)
+                return false;
+
+            if (m_jsonSchema == null)
+            {
+                SchemaBuilder builder = new SchemaBuilder(s_jsonSchemaId);
+                builder.SetSchemaName("IFCExportConfigurationMap");
+                builder.AddSimpleField(s_configMapField, typeof(String));
+                m_jsonSchema = builder.Finish();
+            }
+
+            // It won't start any transaction if there is no change to the configurations
+            if (setupsToSave.Count > 0)
+            {
+                // Overwrite all saved configs with the new list
+                Transaction transaction = new Transaction(document, "Update IFC export setups");
+                try
+                {
+                    transaction.Start("SaveConfigurationChanges");
+                    IList<DataStorage> savedConfigurations = GetSavedConfigurations(m_jsonSchema, document);
+                    int savedConfigurationCount = savedConfigurations.Count<DataStorage>();
+                    int savedConfigurationIndex = 0;
+                    foreach (IFCExportConfiguration configuration in setupsToSave)
+                    {
+                        DataStorage configStorage;
+                        if (savedConfigurationIndex >= savedConfigurationCount)
+                        {
+                            configStorage = DataStorage.Create(document);
+                        }
+                        else
+                        {
+                            configStorage = savedConfigurations[savedConfigurationIndex];
+                            savedConfigurationIndex++;
+                        }
+
+                        Entity mapEntity = new Entity(m_jsonSchema);
+                        string configData = configuration.SerializeConfigToJson();
+                        mapEntity.Set<string>(s_configMapField, configData);
+                        configStorage.SetEntity(mapEntity);
+                    }
+
+                    List<ElementId> elementsToDelete = new List<ElementId>();
+                    for (; savedConfigurationIndex < savedConfigurationCount; savedConfigurationIndex++)
+                    {
+                        DataStorage configStorage = savedConfigurations[savedConfigurationIndex];
+                        elementsToDelete.Add(configStorage.Id);
+                    }
+                    if (elementsToDelete.Count > 0)
+                        document.Delete(elementsToDelete);
+
+                    transaction.Commit();
+                }
+                catch (System.Exception)
+                {
+                    if (transaction.HasStarted())
+                        transaction.RollBack();
+
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
